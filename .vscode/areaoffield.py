25a9611991eebe7f68a3e0388ccb8e2bbf99878a
masonbot <62023052+masonbot@users.noreply.github.com>
2020-04-11 21:04:35 -0400

    Create areaoffield.py

---
+++ .vscode/areaoffield.py
@@ -0,0 +1,11 @@
+Length = input("length of field in feet: ")
+Width = input("Width of field in feet:")
+
+Length = int(Length)
+Width = int(Width)
+
+Areafeet = (Length) * (Width)
+Areafeet = (Areafeet)
+
+Areaacre = Areafeet // 43560
+print(Areaacre) 
