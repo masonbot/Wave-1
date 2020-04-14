2230de335ac7e34c99c66257da3496dc86c302b2
masonbot <62023052+masonbot@users.noreply.github.com>
2020-04-14 13:53:34 -0400

    Update lesson3.py

--- Lesson 3-conditionals/lesson3.py
+++ Lesson 3-conditionals/lesson3.py
@@ -1,11 +1,25 @@
 #Conditionals
 
 #IF statements 
-userVal = input("please enter a number: ")
-userVal = int(userVal)
-num1 = 15
+#userVal = input("please enter a number: ")
+#userVal = int(userVal)
+#num1 = 15
+# Only one of the following conditions will execute
+#if userVal > 15:
+#    print("value os over 15")
+# else if
+#elif userVal < 5:
+#    print("value is very small")
+# Else - no condition, triggers when none of the other options triggers
+#else:
+#   print("value is under 15")
 
-if userVal > 15:
-    print("value os over 15")
-else:
-    print("value is under 15")
+# Comparative operators
+# >, <, >=, <=, ==
+#>  greater than
+#<  less than
+#>  greater than or equal to
+#<  less than or equal to 
+# == equal to
+compare1 = 13 > 12 #Datatype of compare1 is a Bool
+print(compare1)
