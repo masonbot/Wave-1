e164ebed474de8275eccc7978aaba8fd2094377d
masonbot <62023052+masonbot@users.noreply.github.com>
2020-04-14 13:34:03 -0400

    New lesson

---
+++ Lesson 3-conditionals/lesson3.py
@@ -0,0 +1,11 @@
+#Conditionals
+
+#IF statements 
+userVal = input("please enter a number: ")
+userVal = int(userVal)
+num1 = 15
+
+if userVal > 15:
+    print("value os over 15")
+else:
+    print("value is under 15")
