b31993ce4f0d4b6e10609700acfba56337ee8864
masonbot <62023052+masonbot@users.noreply.github.com>
2020-04-14 14:05:28 -0400

    Update lesson3.py

--- Lesson3conditionals/lesson3.py
+++ Lesson3conditionals/lesson3.py
@@ -1,18 +1,18 @@
 #Conditionals
 
 #IF statements 
-#userVal = input("please enter a number: ")
-#userVal = int(userVal)
-#num1 = 15
+userVal = input("please enter a number: ")
+userVal = int(userVal)
+num1 = 15
 # Only one of the following conditions will execute
-#if userVal > 15:
-#    print("value os over 15")
-# else if
-#elif userVal < 5:
-#    print("value is very small")
+if userVal > 15:
+    print("value os over 15")
+#else if
+elif userVal < 5:
+    print("value is very small")
 # Else - no condition, triggers when none of the other options triggers
-#else:
-#   print("value is under 15")
+else:
+   print("value is under 15")
 
 # Comparative operators
 # >, <, >=, <=, ==
